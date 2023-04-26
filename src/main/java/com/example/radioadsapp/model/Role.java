94bc196741249c99f97986b3667a75640a0cabeb
Kudakwashe Kuzvindiwana <kkuzvindiwana@gmail.com>
2023-04-26 21:56:39 +0200

    add authorities to views

--- src/main/java/com/example/radioadsapp/model/Role.java
+++ src/main/java/com/example/radioadsapp/model/Role.java
@@ -36,9 +36,6 @@ public class Role {
 
     @Override
     public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return name;
     }
 }
