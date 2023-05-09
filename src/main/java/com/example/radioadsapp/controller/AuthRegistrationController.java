68b752172388513d5e2936c040182c1ab4baa35d
Kudakwashe Kuzvindiwana <kkuzvindiwana@gmail.com>
2023-05-09 08:13:17 +0200

    fix login

--- src/main/java/com/example/radioadsapp/controller/AuthRegistrationController.java
+++ src/main/java/com/example/radioadsapp/controller/AuthRegistrationController.java
@@ -66,7 +66,7 @@ public class AuthRegistrationController {
             return "redirect:/register?error=Invalid email address";
         // save user to database
         userService.saveUser(userDto);
-        return "redirect:/login?success";
+        return "redirect:/";
 
 
     }
