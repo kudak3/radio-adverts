bd184e8bb4c05baff22fbaeb97b1cabb7dafe3e3
Kudakwashe Kuzvindiwana <kkuzvindiwana@gmail.com>
2023-05-09 07:57:44 +0200

    rename fn and add email validation

--- src/main/java/com/example/radioadsapp/utils/Util.java
+++ src/main/java/com/example/radioadsapp/utils/Util.java
@@ -2,10 +2,12 @@ package com.example.radioadsapp.utils;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public class FileUploadUtil {
+public class Util {
 
     public static void saveFile(String uploadDir, String fileName,
                                 MultipartFile multipartFile) throws IOException {
@@ -21,5 +23,13 @@ public class FileUploadUtil {
         } catch (IOException ioe) {
             throw new IOException("Could not save image file: " + fileName, ioe);
         }
+    }
+
+
+    public static boolean isValidEmail(String email)
+    {
+         Pattern emailPattern = Pattern.compile("[a-zA-Z0-9[!#$%&'()*+,/\\-_\\.\"]]+@[a-zA-Z0-9[!#$%&'()*+,/\\-_\"]]+\\.[a-zA-Z0-9[!#$%&'()*+,/\\-_\"\\.]]+");
+        Matcher m = emailPattern.matcher(email);
+        return !m.matches();
     }
 }
